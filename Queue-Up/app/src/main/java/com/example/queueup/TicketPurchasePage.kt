af447c167a24cea143d2bc6ec6fc47df8761b6be
taheriali4 <34661211+taheriali4@users.noreply.github.com>
2022-10-23 19:15:20 -0400

    Update TicketPurchasePage.kt

--- Queue-Up/app/src/main/java/com/example/queueup/TicketPurchasePage.kt
+++ Queue-Up/app/src/main/java/com/example/queueup/TicketPurchasePage.kt
@@ -1,4 +1,36 @@
 package com.example.queueup
 
+import android.os.CountDownTimer
+
 class TicketPurchasePage {
+  
+  class
+  object : CountDownTimer(30000, 1000) {
+ 
+      override fun onTick(millisUntilFinished: Long) {
+          mTextField.setText("seconds remaining: " + millisUntilFinished / 1000)
+      }
+ 
+      override fun onFinish() {
+          mTextField.setText("done!")
+      }
+  }.start()
+  
+  private fun setTimerEnabled(isInForeground: Boolean) {
+        if (isInForeground) {
+
+            // Update the counter and post the mUpdater Runnable
+            with(binding.counter) {
+                text = counter.toString()
+                postDelayed(updater, DELAY)
+            }
+        } else {
+
+            // Remove already posted Runnables to stop the ticker's updating
+            binding.counter.removeCallbacks(updater)
+        }
+    }
+
+  
+  
 }
